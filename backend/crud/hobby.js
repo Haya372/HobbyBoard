1d2f89a45123904d8eda523194e3c9fdb9fa2166
toshikingggg <study.king777@gmail.com>
2020-10-06 23:07:07 +0900

    趣味詳細へいいね、よくないねの機能追加

--- backend/crud/hobby.js
+++ backend/crud/hobby.js
@@ -19,8 +19,46 @@ const read_hobby = async function(hobby_id, res){
         hobby: hobby[0][0],
         comments: comment[0]
     });
+     console.log("hobby[0][0].good:",hobby[0][0].good)
+};
+
+
+
+const good_hobby = async function(hobby_id, res){
+    let hobby = await models.sequelize.query(
+        `select Hobbies.id as id, user_id, username, title, content, good, bad, secret
+        from Hobbies
+        inner join Users
+        on Users.id=user_id
+        where Hobbies.id=` + hobby_id
+    );
+    
+    // //いいね
+    hobby[0][0].good += 1;
+    res.status(200).send({
+        hobby: hobby[0][0],
+    });
 }
 
+const bad_hobby = async function(hobby_id, res){
+    let hobby = await models.sequelize.query(
+        `select Hobbies.id as id, user_id, username, title, content, good, bad, secret
+        from Hobbies
+        inner join Users
+        on Users.id=user_id
+        where Hobbies.id=` + hobby_id
+    );
+    
+    //よくないね
+    hobby[0][0].bad += 1;
+    res.status(200).send({
+        hobby: hobby[0][0],
+    });
+}
+
+
 module.exports = {
-    read_hobby
+    read_hobby,
+    good_hobby,
+    bad_hobby
 }
--- backend/routes/hobby.js
+++ backend/routes/hobby.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const crud_hobby = require('../crud/hobby');
+const crud_good_hobby = require('../crud/hobby');
+const crud_bad_hobby = require('../crud/hobby');
 
 router.get('/:id', function(req, res, next){
     let id = req.params.id;
     crud_hobby.read_hobby(id, res);
+});
+
+//いいねの更新
+router.put('/good/:id',(req, res) => {
+    //idに紐付いたhobbyデータを取る
+    let id = req.params.id;
+    crud_good_hobby.good_hobby(id, res);
+});
+
+router.put('/bad/:id',(req, res) => {
+    //idに紐付いたhobbyデータを取る
+    let id = req.params.id;
+    crud_bad_hobby.bad_hobby(id, res);
 });
 
 module.exports = router;
